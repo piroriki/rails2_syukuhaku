7e10e860c4b9dea4763c37426a3eff60cb32a6e4
弘瀬史歩 <hiroseshiho@hiroseshihonoMacBook-Air.local>
2022-11-07 22:28:17 +0900

    パーシャル不具合質問用コミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users,controllers: {
+   registrations: 'users/registrations',
+   sessions: 'users/sessions'
+}
+
+  devise_scope :user do
+   get 'users/sign_up',to: 'users/registrations#new'
+   get 'users/sign_in',to: 'users/sessions#new' 
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
